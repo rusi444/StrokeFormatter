672c72fe8009e8b78d99249f27eab7a091f14668
rusi444 <L3or5@mail.ru>
2024-05-14 18:40:55 +0600

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,78 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите код для форматирования:");
+        String inputCode = sc.nextLine();
+
+        String formattedCode = formatCode(inputCode);
+
+        System.out.println("Отформатированный код:");
+        System.out.println(formattedCode);
     }
+
+    public static String formatCode(String code)
+    {
+        StringBuilder formattedCode = new StringBuilder();
+        int indentationLevel = 0;
+
+        for (int i = 0; i < code.length(); i++)
+        {
+            char c = code.charAt(i);
+
+            if (c == '{' || c-1 == '[')
+            {
+                formattedCode.append("\n");
+                for (int j = 0; j < indentationLevel; j++)
+                {
+                    formattedCode.append("\t");
+                }
+                formattedCode.append(c);
+                formattedCode.append("\n");
+                indentationLevel++;
+                for (int j = 0; j < indentationLevel; j++)
+                {
+                    formattedCode.append("\t");
+                }
+            }
+
+            else if (c == ';' && c != '}')
+            {
+                formattedCode.append(c);
+                formattedCode.append("\n");
+                for (int j = 0; j < indentationLevel; j++)
+                {
+                    formattedCode.append("\t");
+                }
+            }
+
+            else if (c == '}' || c+1 == '}')
+            {
+                formattedCode.append("\n");
+                indentationLevel--;
+                for (int j = 0; j < indentationLevel; j++)
+                {
+                    formattedCode.append("\t");
+                }
+                formattedCode.append(c);
+                formattedCode.append("\n");
+                for (int j = 0; j < indentationLevel; j++)
+                {
+                    formattedCode.append("\t");
+                }
+            }
+
+            else
+            {
+                formattedCode.append(c);
+            }
+        }
+
+        return formattedCode.toString();
+    }
+
 }
